913b1bd528b42b5f4f7ebfc5dd780d699cd7fd75
PS-SemiQa <201234504+PS-SemiQa@users.noreply.github.com>
2026-04-20 17:25:54 +0200

    Change the way of driving inputs and checking outputs - no longer using indexing.

--- test/test.py
+++ test/test.py
@@ -26,15 +26,14 @@ async def test_project(dut):
     dut._log.info("Test project behavior")
 
     # Set the input values you want to test
-    dut.ui_in[0].value = 1
-    dut.ui_in[1].value = 0
+    dut.ui_in.value = 3
 
     # Wait for one clock cycle to see the output values
     await ClockCycles(dut.clk, 1)
 
     # The following assersion is just an example of how to check the output values.
     # Change it to match the actual expected output of your module:
-    assert dut.uo_out[3].value == 1     # Failure output should not get activated
+    assert dut.uo_out.value & 8 == 1     # Failure output should not get activated
 
     # Keep testing the module by changing the input values, waiting for
     # one or more clock cycles, and asserting the expected output values.
